0bb5ea0cab7a8a3606fd733ec697783f28186e90
dgowtham97 <100546723+dgowtham97@users.noreply.github.com>
2022-06-08 12:44:49 -0700

    Code to create .gif from images

---
+++ GIF_Creating_Python.py
@@ -0,0 +1,14 @@
+import glob
+from PIL import Image
+a = glob.glob(r"C:/ASU/Spring 2021/PARA Lab/rgb/*.png");
+print(*a, sep="\n")
+frame = []
+c = len(a)
+print(c)
+for pics in a:
+    framenew = Image.open(pics)
+    frame.append(framenew)
+frame[0].save('C:/ASU/Spring 2021/PARA Lab/rgb/GIF.gif', save_all=True, append_images= frame[1:], optimize = False, duration=10, loop=0)
+
+
+   
