2ba5c777c3300cde2a687291446884f8dbdb543e
dgowtham97 <100546723+dgowtham97@users.noreply.github.com>
2022-09-03 15:41:10 -0700

    Update GIF_Creating_Python.py

--- GIF_Creating_Python.py
+++ GIF_Creating_Python.py
@@ -8,6 +8,7 @@ print(c)
 for pics in a:
     framenew = Image.open(pics)
     frame.append(framenew)
+    #Saving GIF
 frame[0].save('C:/ASU/Spring 2021/PARA Lab/rgb/GIF.gif', save_all=True, append_images= frame[1:], optimize = False, duration=10, loop=0)
 
 
